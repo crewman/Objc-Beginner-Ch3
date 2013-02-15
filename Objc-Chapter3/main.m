ad2362c49a0e1ad885415e4328da5112e7a5c10f
Leif Petersen <leifthelucky@gmail.com>
2013-02-14 19:22:02 -0800

    So we went from the basic file to what we're playing with now...classes! I have no idea what a class is, is it a function? Is it  a plane...

--- Objc-Chapter3/main.m
+++ Objc-Chapter3/main.m
@@ -1,22 +1,70 @@
-//
-//  main.m
-//  Objc-Chapter3
-//
-//  Created by Leif Petersen on 13-02-14.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
-//
+/*
+ 
+ Introduction to Classes, and Objects. 
+ This is to create a Fraction Class that takes a numerator and denominator, and then prints it as in the n/d format. Which isn't very useful to be sure, but it's a start!
+ 
+ 
+ */
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+// @interface section
 
+@interface Fraction: NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+
+// @implementation section
+
+@implementation Fraction
+        {
+    int numerator, denominator;
+    
+}
+-(void) print
+{
+NSLog(@"%i/%i",numerator,denominator);
+}
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+    
+}
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+// Program section - hurray!
+
+int main (int argc, char * argv[])
+{
     @autoreleasepool {
+        Fraction *myFraction;
+        // time to create an instace of a Fraction!
         
-        // insert code here...
-        NSLog(@"Rawr!");
+        myFraction = [Fraction alloc];
+        myFraction = [Fraction init];
+        
+        // set to 1/4
+        
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator:4];
+        
+        // Display my precious! DISPLAY I TELL YOU!
+        
+        NSLog(@"The Value of myFraction is");
+        [myFraction print];
         
     }
     return 0;
 }
+
 
